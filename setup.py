638f3247a41d61dc03747e88cb1f2b5525795aa1
Volobueva-Yuliya <pusifon@gmail.com>
2022-08-17 13:25:04 +0400

    setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='dcpsfrgan',
+    version='0.1dev',
+    packages=find_packages(),
+    license='',
+    python_requires=">=3.8.*",
+    install_requires=[
+	torch,
+	torchvision,
+	tensorflow>=1.15.4,
+	tensorboard,
+	tensorboardX,
+	opencv-python,
+	dlib,
+	scikit-image,
+	scipy,
+	tqdm,
+	imgaug,
+    ],
+    include_package_data=True,
+    package_data={'': ['*.yaml']},
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown'
+)
